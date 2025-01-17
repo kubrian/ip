9f2dd2ba2ba3dce8cccc38c6b020e0289d595953
Brian Ku <48638231+kubrian@users.noreply.github.com>
2025-01-17 13:53:38 +0800

    Add support to echo user input

--- src/main/java/Luna.java
+++ src/main/java/Luna.java
@@ -1,22 +1,50 @@
+import java.util.Scanner;
+
 public class Luna {
     public static final String NAME = "Luna";
+    public static final String GREETING = "Hello! I'm " + NAME + "!\nWhat can I do for you?\n";
+    public static final String BYE = "Bye. Hope to see you again soon!\n";
+    public static final Scanner scanner = new Scanner(System.in);
+
 
     public static void main(String[] args) {
-        greet();
+        // Greet the user, interact until the user says bye
+        System.out.println(GREETING);
+        interact();
+        System.out.println(BYE);
+        scanner.close();
     }
 
     /**
-     * Initial greeting
+     * Formats the chatbot response such that it can be printed to the console.
+     * <p>
+     * Appends the name of the program to the beginning of the response
+     *
+     * @param response The response from the chatbot
      */
-    public static void greet() {
-        String header = """
-                ____________________________________________________________
-                Hello! I'm %s
-                What can I do for you?
-                ____________________________________________________________
-                Bye. Hope to see you again soon!
-                ____________________________________________________________""";
+    public static String formatResponse(String response) {
+        return String.format("%s: %s\n", NAME, response);
+    }
 
-        System.out.printf(header, NAME);
+
+    /**
+     * Reads input from the user and prints out a response until the user
+     * enters "bye".
+     * <p>
+     * The response is simply the input given by the user.
+     */
+    public static void interact() {
+        while (true) {
+            // Read input
+            System.out.print("> ");
+            String input = scanner.nextLine();
+
+            // Check if the user wants to exit
+            if (input.equals("bye")) {
+                break;
+            }
+
+            System.out.println(formatResponse(input));
+        }
     }
 }
