6f52b74d8a958941a74d2cddffab1c8c9466052c
Brian Ku <48638231+kubrian@users.noreply.github.com>
2025-01-24 15:00:11 +0800

    Refactor loading and writing file into Storage

--- src/main/java/Luna.java
+++ src/main/java/Luna.java
@@ -1,9 +1,5 @@
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.ArrayList;
 
 public class Luna {
@@ -23,55 +19,26 @@ public class Luna {
 
     // I/O
     private final ConsoleUI consoleUi;
+    private final Storage storage;
 
     // Data
     private final ArrayList<Task> taskList;
 
-    public Luna() {
+    public Luna(String saveFileName) {
         this.consoleUi = new ConsoleUI(NAME, GREETING, BYE, HELP, UNSUPPORTED, INCOMPLETE);
         this.taskList = new ArrayList<>();
+        this.storage = new Storage(saveFileName);
     }
 
     public static void main(String[] args) {
-        Luna bot = new Luna();
-        bot.loadTasksFromFile();
-        bot.greetUser();
+        Luna bot = new Luna(saveFileName);
         bot.run();
     }
 
-    private void loadTasksFromFile() {
-        try {
-            BufferedReader br = new BufferedReader(new FileReader(saveFile));
-            String line;
-            consoleUi.printOutput("Loading tasks from data file...");
-            while ((line = br.readLine()) != null) {
-                String[] comp = line.split(" ", 2);
-                // Task type
-                Parser.parseInput(comp[1])
-                      .execute(consoleUi, null, taskList);
-                // Completion
-                if (comp[0].equals("1")) {
-                    // Mark last task as completed
-                    new MarkCommand(taskList.size()).execute(consoleUi, null, taskList);
-                }
-            }
-            br.close();
-            consoleUi.printOutput("");
-            consoleUi.printOutput("Loaded " + taskList.size() + " tasks.");
-            new ListCommand().execute(consoleUi, null, taskList);
-            consoleUi.printOutput("");
-        } catch (IOException e) {
-            consoleUi.printOutput("Unable to load tasks from file.");
-        }
-    }
-
-    private void greetUser() {
-        consoleUi.greetUser();
-    }
-
     public void run() {
-        while (interact()) {
-        }
+        storage.loadTasksFromFile(consoleUi, taskList);
+        consoleUi.greetUser();
+        interactWithUser();
         consoleUi.close();
     }
 
@@ -80,49 +47,31 @@ public class Luna {
      * <p>
      * Returns false if the user says bye, true otherwise.
      */
-    private boolean interact() {
+    private void interactWithUser() {
         // Read input
-        String input = consoleUi.getInput();
-        Command command;
-        try {
-            command = Parser.parseInput(input);
-        } catch (IllegalArgumentException e) {
-            consoleUi.printOutput(e.getMessage());
-            consoleUi.printOutput(HELP);
-            return true;
-        }
+        while (true) {
+            String input = consoleUi.getInput();
+            Command command;
+            try {
+                command = Parser.parseInput(input);
+            } catch (IllegalArgumentException e) {
+                consoleUi.printOutput(e.getMessage());
+                consoleUi.printOutput(HELP);
+                continue;
+            }
 
-        try {
-            boolean cont = command.execute(consoleUi, null, taskList);
-            saveTasksToFile();
-            return cont;
-        } catch (FileNotFoundException e) {
-            consoleUi.printOutput("Failed to save tasks to file");
-        } catch (IndexOutOfBoundsException e) {
-            consoleUi.printOutput("Invalid task number");
+            try {
+                boolean cont = command.execute(consoleUi, storage, taskList);
+                storage.saveTasksToFile(taskList);
+                if (!cont) {
+                    break;
+                }
+            } catch (FileNotFoundException e) {
+                consoleUi.printOutput("Failed to save tasks to file");
+            } catch (IndexOutOfBoundsException e) {
+                consoleUi.printOutput("Invalid task number");
+            }
         }
-
-        return true;
-    }
-
-    /**
-     * Saves the current list of tasks to a file.
-     * <p>
-     * The file format is newline-separated, each line consisting of completion status and task
-     * description.
-     */
-    private void saveTasksToFile() throws FileNotFoundException {
-        // Ensure directory exists
-        File dir = saveFile.getParentFile();
-        if (!dir.exists()) {
-            dir.mkdirs();
-        }
-        // Write to file
-        PrintWriter pw = new PrintWriter(saveFile);
-        taskList.stream()
-                .map(task -> (task.isCompleted() ? 1 : 0) + " " + task.getCommandString())
-                .forEach(pw::println);
-        pw.close();
     }
 
 }
