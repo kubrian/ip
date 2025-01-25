b70fb496a46e0c2f8d6005ee94c957c5496f5c31
Brian Ku <48638231+kubrian@users.noreply.github.com>
2025-01-26 00:32:51 +0800

    Add Javadoc

--- src/main/java/luna/task/Deadline.java
+++ src/main/java/luna/task/Deadline.java
@@ -2,18 +2,24 @@ package luna.task;
 
 import java.time.LocalDateTime;
 
+/**
+ * Represents a deadline task.
+ */
 public class Deadline extends Task {
 
     private final LocalDateTime by;
 
+    /**
+     * Creates a new deadline task.
+     *
+     * @param description The description of this task
+     * @param by          The deadline of this task
+     */
     public Deadline(String description, LocalDateTime by) {
         super(description);
         this.by = by;
     }
 
-    /**
-     * @inheritDoc
-     */
     public String getCommandString() {
         return String.format("deadline %s /by %s", getDescription(),
                 by.format(inputDateTimeFormatter));
--- src/main/java/luna/ui/ConsoleUi.java
+++ src/main/java/luna/ui/ConsoleUi.java
@@ -10,24 +10,41 @@ public class ConsoleUi {
     private final Scanner scanner;
     private final String BYE;
 
+    /**
+     * Creates a new ConsoleUi.
+     *
+     * @param bye The message to print upon exit.
+     */
     public ConsoleUi(String bye) {
         BYE = bye;
         scanner = new Scanner(System.in);
     }
 
+    /**
+     * Closes the scanner.
+     */
     public void close() {
         scanner.close();
     }
 
+    /**
+     * Prompts the user for input and returns it.
+     */
     public String getInput() {
         System.out.print("> ");
         return scanner.nextLine();
     }
 
+    /**
+     * Prints the exit message to the user.
+     */
     public void goodbye() {
         printOutput(BYE);
     }
 
+    /**
+     * Prints the given message to the user.
+     */
     public void printOutput(String output) {
         System.out.println(output);
     }
